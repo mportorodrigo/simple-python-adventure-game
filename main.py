a1f8cbd04f59fb69e016034a17ac5422f76cd6df
mportorodrigo <mportorodrigo@outlook.com>
2019-05-25 17:51:13 -0300

    write validate_option

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ def print_sleep(text):
     time.sleep(2)
 
 
+def validate_option(options):
+    # Print all the option for the player to choose from
+    i = 0
+    while i < len(options):
+        print(f"{i}. {options[i]}")
+        i += 1
+
+    # Verify and returns the player choice
+    while True:
+        selected_option = input("What would you like to do?\n")
+        selected_option = int(selected_option)
+
+        if selected_option > 0 and selected_option < len(options) + 1:
+            return selected_option
+        else:
+            print_sleep("That is not a valid option.")
+
+
 # Describes the setting and start of the game
 def intro():
     print_sleep("You wake up, in a cell, hearing a moaning.")
